ae692eec2d92674f0e921a4844f327c8326e4124
Ananda Darma <anandadarma2612@gmail.com>
2025-01-07 10:51:48 +0800

    feat: add master data route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DistrictController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\MasterDataController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DistrictController::class, 'index'])->name('landing-page');
@@ -13,6 +14,10 @@ Route::get('/dashboard', function () {
     return view('pages.admin.dashboard');
 });
 
-Route::get('/master-data', function () {
-    return view('pages.admin.master_data');
-});
+Route::get('/master-data', [MasterDataController::class, 'index'])->name('master-data.index');
+Route::get('/master-data/create', [MasterDataController::class, 'create'])->name('master-data.create');
+Route::post('/master-data', [MasterDataController::class, 'store'])->name('master-data.store');
+Route::get('/master-data/{id}', [MasterDataController::class, 'show'])->name('master-data.show');
+Route::get('/master-data/{id}/edit', [MasterDataController::class, 'edit'])->name('master-data.edit');
+Route::put('/master-data/{id}', [MasterDataController::class, 'update'])->name('master-data.update');
+Route::delete('/master-data/{id}', [MasterDataController::class, 'destroy'])->name('master-data.destroy');
