176a47d069dbc4762b80e5c61ad9e9de72899c0d
Ananda Darma <anandadarma2612@gmail.com>
2025-01-09 03:21:53 +0800

    feat: handling hamburger button for responsive

--- resources/views/components/partials/sidebar.blade.php
+++ resources/views/components/partials/sidebar.blade.php
@@ -19,7 +19,7 @@
                         class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">GovCall</span>
                 </a>
             </div>
-            <div class="flex items-center">
+            {{-- <div class="flex items-center">
                 <div class="flex items-center ms-3">
                     <div>
                         <button type="button"
@@ -64,7 +64,7 @@
                         </ul>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 </nav>
@@ -100,29 +100,30 @@
                 </a>
             </li>
             <li>
-                <a href="#"
-                    class="flex items-center p-3 text-gray-900 rounded-lg dark:text-white hover:bg-blue-500 group">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                        <path
-                            d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-                    </svg>
-                    <span class="flex-1 ms-3 whitespace-nowrap">Admin</span>
-                </a>
-            </li>
-            <li>
-                <a href="#"
-                    class="flex items-center p-3 text-gray-900 rounded-lg dark:text-white hover:bg-blue-500 group">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" aria-hidden="true">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4m10 9h-6m0 0 3-3m-3 3 3 3" />
-                    </svg>
-
-
-                    <span class="flex-1 ms-3 whitespace-nowrap">Log Out</span>
-                </a>
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit" class="flex w-full items-start justify-start p-3 text-gray-900 rounded-lg dark:text-white hover:bg-red-500 group hover:text-white">
+                        <svg class="flex w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" aria-hidden="true">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4m10 9h-6m0 0 3-3m-3 3 3 3" />
+                        </svg>
+                        <span class="ms-3 whitespace-nowrap">Log Out</span>
+                    </button>
+                </form>
             </li>
         </ul>
     </div>
 </aside>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const mobileMenuButton = document.querySelector('[data-drawer-toggle="logo-sidebar"]');
+        const sidebar = document.getElementById('logo-sidebar');
+        
+        mobileMenuButton.addEventListener('click', function() {
+            sidebar.classList.toggle('-translate-x-full');
+        });
+    });
+</script>
+
