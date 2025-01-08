524e76aef2af3fd1849579827dd4b608f34ee57c
Ananda Darma <anandadarma2612@gmail.com>
2025-01-09 03:18:41 +0800

    feat: handling hamburger button for responsive

--- resources/views/components/partials/navbar.blade.php
+++ resources/views/components/partials/navbar.blade.php
@@ -6,7 +6,7 @@
                 <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">GovCall</span>
             </div>
 
-            <div class="hidden sm:flex space-x-4 items-center justify-center flex-1">
+            <div class="hidden sm:flex space-x-4 items-end justify-end flex-1">
                 <a href="#home"
                     class="rounded-md px-3 py-2 text-[18px] text-white hover:bg-blue-500 hover:text-white">Home</a>
                 <a href="#about"
@@ -15,11 +15,6 @@
                     class="rounded-md px-3 py-2 text-[18px] text-white hover:bg-blue-500 hover:text-white">Service</a>
                 <a href="#faq"
                     class="rounded-md px-3 py-2 text-[18px] text-white hover:bg-blue-500 hover:text-white">FAQ</a>
-            </div>
-
-            <div class="hidden sm:flex items-center">
-                <a href=""
-                    class="rounded-md bg-blue-500 w-[110px] h-[40px] flex items-center justify-center text-[18px] text-white hover:bg-blue-600">Login</a>
             </div>
 
             <div class="sm:hidden flex items-center">
@@ -44,39 +39,13 @@
                 class="block rounded-md px-3 py-2 text-[18px] text-white hover:bg-blue-500 hover:text-white">Service</a>
             <a href="#faq"
                 class="block rounded-md px-3 py-2 text-[18px] text-white hover:bg-blue-500 hover:text-white">FAQ</a>
-            <a href=""
-                class="block rounded-md bg-blue-500 px-3 py-2 text-[18px] text-center text-white hover:bg-blue-600">Login</a>
         </div>
     </div>
 </nav>
 
-{{-- <script>
-    window.addEventListener('scroll', function() {
-        const navbar = document.getElementById('navbar');
-        if (window.scrollY > 50) {
-            navbar.classList.add('bg-[#213555]');
-            navbar.classList.remove('bg-transparent');
-        } else {
-            navbar.classList.add('bg-transparent');
-            navbar.classList.remove('bg-[#213555]');
-        }
-    });
-
-    const menuButton = document.getElementById('mobile-menu-button');
-    const mobileMenu = document.getElementById('mobile-menu');
-    const navbar = document.getElementById('navbar');
-
-    menuButton.addEventListener('click', function() {
+<script>
+    document.getElementById('mobile-menu-button').addEventListener('click', function() {
+        const mobileMenu = document.getElementById('mobile-menu');
         mobileMenu.classList.toggle('hidden');
-
-        if (!mobileMenu.classList.contains('hidden')) {
-            navbar.classList.add('bg-[#213555]');
-            navbar.classList.remove('bg-transparent');
-        } else {
-            if (window.scrollY <= 50) {
-                navbar.classList.remove('bg-[#213555]');
-                navbar.classList.add('bg-transparent');
-            }
-        }
     });
-</script> --}}
+</script>
