e8d7eca5bd7127f209bff2cf1ac06a31605ef1c0
Ananda Darma <anandadarma2612@gmail.com>
2025-01-09 02:21:06 +0800

    fix: dashboard styling

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -1,6 +1,4 @@
-@extends('components.layouts.main')
-
-@include('components.partials.sidebar')
+@extends('components.layouts.main_admin')
 
 @section('container')
     <section class="px-5 md:pl-24 md:pr-20 my-10 md:ml-56 flex-grow mt-[100px]">
@@ -10,30 +8,30 @@
 
         <div class="grid grid-cols-1 md:grid-cols-4 gap-6 mb-6">
             <div class="bg-white p-6 rounded-lg shadow">
-                <h2 class="text-xl font-bold text-gray-700">Total Data</h2>
+                <h1 class="text-xl md:text-2xl font-bold text-gray-700">Total Data</h1>
                 <p class="text-3xl font-semibold text-blue-500 mt-2">{{ $totalData }}</p>
             </div>
             <div class="bg-white p-6 rounded-lg shadow">
-                <h2 class="text-xl font-bold text-gray-700">Active Districts</h2>
+                <h1 class="text-xl md:text-2xl font-bold text-gray-700">Active Districts</h1>
                 <p class="text-3xl font-semibold text-green-500 mt-2">{{ $activeDistricts }}</p>
             </div>
             <div class="bg-white p-6 rounded-lg shadow">
-                <h2 class="text-xl font-bold text-gray-700">Active Sub-Districts</h2>
+                <h1 class="text-xl md:text-2xl font-bold text-gray-700">Active Sub-Districts</h1>
                 <p class="text-3xl font-semibold text-green-500 mt-2">{{ $activeSubDistricts }}</p>
             </div>
             <div class="bg-white p-6 rounded-lg shadow">
-                <h2 class="text-xl font-bold text-gray-700">New Data This Month</h2>
+                <h1 class="text-xl md:text-2xl font-bold text-gray-700">New Data This Month</h1>
                 <p class="text-3xl font-semibold text-yellow-500 mt-2">{{ $newDataThisMonth }}</p>
             </div>
         </div>
 
         <div class="bg-white p-6 rounded-lg shadow">
-            <h2 class="text-xl font-bold text-gray-700 mb-4">Recent Data Entries</h2>
+            <h1 class="text-xl md:text-2xl font-bold text-gray-700 mb-4">Recent Data Entries</h1>
             <ul class="divide-y divide-gray-200">
                 @foreach ($recentContacts as $contact)
                     <li class="py-4 flex justify-between items-center">
                         <div>
-                            <h3 class="text-lg font-semibold text-gray-700">{{ $contact->office_name }} -
+                            <h3 class="text-lg md:text-xl font-semibold text-gray-700">{{ $contact->office_name }} -
                                 {{ $contact->district->name }}</h3>
                             <p class="text-gray-500">{{ $contact->sub_district->name }},
                                 {{ $contact->created_at->diffForHumans() }}</p>
